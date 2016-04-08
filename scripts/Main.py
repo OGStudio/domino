8c542d74e3a640cf16163798e7e92b3fe91f6481
Michael Kapelko <kornerr@gmail.com>
2016-04-08 09:22:26 +0700

    Main start

--- scripts/Main.py
+++ scripts/Main.py
@@ -1,30 +1,31 @@
 
 from pymjin2 import *
 
-MAIN_BALL_NAME   = "ball"
 MAIN_SOUND_START = "soundBuffer.default.start"
 
 class MainImpl(object):
     def __init__(self, c):
         self.c = c
+        self.isStarted = False
     def __del__(self):
         self.c = None
-    def onBallStopped(self, key, value):
-        print "Ball stopped"
-    def onSpace(self, key, value):
-        print "Space pressed. Replay sound. Rotate the ball"
+    def onStart(self, key, value):
+        if (self.isStarted):
+            return
+        self.isStarted = True
+        print "Start the game"
         self.c.set("$SNDSTART.state", "play")
-        self.c.set("$BALL.$SCENE.$BALL.moving", "1")
+        #self.c.set("$BALL.$SCENE.$BALL.moving", "1")
 
 class Main(object):
     def __init__(self, sceneName, nodeName, env):
         self.c = EnvironmentClient(env, "Main")
         self.impl = MainImpl(self.c)
         self.c.setConst("SCENE",    sceneName)
-        self.c.setConst("BALL",     MAIN_BALL_NAME)
+        #self.c.setConst("BALL",     MAIN_BALL_NAME)
         self.c.setConst("SNDSTART", MAIN_SOUND_START)
-        self.c.listen("input.SPACE.key", "1", self.impl.onSpace)
-        self.c.listen("$BALL.$SCENE.$BALL.moving", "0", self.impl.onBallStopped)
+        self.c.listen("input.SPACE.key", "1", self.impl.onStart)
+        #self.c.listen("$BALL.$SCENE.$BALL.moving", "0", self.impl.onBallStopped)
     def __del__(self):
         # Tear down.
         self.c.clear()

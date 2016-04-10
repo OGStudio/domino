fa7c9eb4715868a566fe463b0af80dc14e3c5bd3
Michael Kapelko <kornerr@gmail.com>
2016-04-10 17:04:23 +0700

    #2345 Construct domino draft - Source rotates correctly

--- scripts/Source.py
+++ scripts/Source.py
@@ -10,15 +10,29 @@ SOURCE_TILE_FACTORY     = "tileFactory"
 class SourceImpl(object):
     def __init__(self, c):
         self.c = c
-        self.tiles = []
+        self.rotationSpeed = None
+        self.tiles = {}
     def __del__(self):
         self.c = None
     def onTileSelection(self, key, value):
-        print "onTileSelection", key, value
+        tileName = key[1]
+        self.prepareRotation(tileName)
         self.c.set("$ROTATE.$SCENE.$NODE.active", "1")
+    def prepareRotation(self, tileName):
+        # The first call.
+        if (self.rotationSpeed is None):
+            point = self.c.get("$ROTATE.point")[0]
+            v = point.split(" ")
+            self.rotationSpeed = int(v[0])
+        # Destination rotation = -90 - 60 * slot.
+        for slot, tile in self.tiles.items():
+            if (tile == tileName):
+                point = "{0} 0 0 {1}".format(self.rotationSpeed,
+                                             -90 - 60 * slot)
+                self.c.set("$ROTATE.point", point)
     def setReset(self, key, value):
         # Create 6 tiles.
-        for i in xrange(1, 7):
+        for i in xrange(0, 6):
             name = self.c.get("$TF.newTile")[0]
             # Change parent to corresponding tile leaf.
             self.c.setConst("NAME", name)
@@ -26,7 +40,8 @@ class SourceImpl(object):
             self.c.set("node.$SCENE.$NAME.parent",     parent)
             self.c.set("node.$SCENE.$NAME.selectable", "1")
             self.c.set("$TILE.$NAME.plate",            SOURCE_NAME)
-            self.tiles.append(name)
+            # Each tile is in its designated slot.
+            self.tiles[i] = name
             # Listen to tile selection.
             self.c.listen("$TILE.$NAME.selected", "1", self.onTileSelection)
 
